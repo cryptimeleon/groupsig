fa7b0b6255d59d98bbb3a850b81192893b46b523
Raphael Heitjohann <5891816+rheitjoh@users.noreply.github.com>
2021-04-07 09:19:38 +0200

    Update javadocs for ClaimProof and OpenProof regarding representation and change access modifiers in OpenResult

--- src/main/java/org/cryptimeleon/groupsig/common/ClaimProof.java
+++ src/main/java/org/cryptimeleon/groupsig/common/ClaimProof.java
@@ -6,7 +6,7 @@ import org.cryptimeleon.math.serialization.Representable;
  * A claim proof proves that a {@link GroupSignature} was issued by specific member of
  * the group. It can be verified using {@link GroupSignatureScheme#claimVerify(ClaimProof, GroupSignature)}.
  *
- * <p>It can be restored from its representation via the appropriate method from {@link GroupSignatureScheme}.
+ * <p>It can be restored from its representation via the appropriate method from {@link GroupSignature}.
  */
 public interface ClaimProof extends Representable {
 }
